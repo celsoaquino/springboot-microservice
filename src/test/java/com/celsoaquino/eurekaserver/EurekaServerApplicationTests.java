4950e018ac4c3b9e11c5faa56e699b88feabca7e
Celso Aquino <celso.aquino@ymail.com>
2020-06-04 21:04:14 -0300

    create eureka server

---
+++ src/test/java/com/celsoaquino/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celsoaquino.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
